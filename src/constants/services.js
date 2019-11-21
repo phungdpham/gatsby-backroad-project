922722ea62b450353e9c5c99f4aee500c8d0e84c
Phung Pham <phung.tech@gmail.com>
2019-11-20 20:32:34 -0600

    first commit

---
+++ src/constants/services.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { FaWallet, FaTree, FaSocks } from "react-icons/fa"
+
+export default [
+  {
+    icon: <FaWallet />,
+    title: "saving money",
+    text:
+      "It is a long established fact that a reader will be distracted by he readable content of a page when looking at its layout.",
+  },
+  {
+    icon: <FaTree />,
+    title: "endless hiking",
+    text:
+      "It is a long established fact that a reader will be distracted by he readable content of a page when looking at its layout.",
+  },
+  {
+    icon: <FaSocks />,
+    title: "amazing comfort",
+    text:
+      "It is a long established fact that a reader will be distracted by he readable content of a page when looking at its layout.",
+  },
+]
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,24 @@
 import React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/Layout"
+import SimpleHero from "../components/SimpleHero"
+import Banner from "../components/Banner"
+import About from '../components/Home/About'
+import Services from '../components/Home/Services'
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <Layout>
+    <SimpleHero>
+      <Banner
+        title="continue exploring"
+        info="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. "
+      >
+        <Link to="/tours" className="btn-white">
+          explore tours
+        </Link>
+      </Banner>
+    </SimpleHero>
+    <About />
+    <Services />
+  </Layout>
+)
