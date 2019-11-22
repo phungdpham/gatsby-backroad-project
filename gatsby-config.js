8f390d80d1356ac3d2ccb007b628a0c0c89f9ad5
Phung Pham <phung.tech@gmail.com>
2019-11-21 21:43:51 -0600

    env variables added

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,17 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
-  siteMetadata:{
-    title:"BackRoads",
-    description:"Explore awesome worldwide tours & discover what makes each of them unique. Forget your daily routing & say yes to adventure",
-    author:'@johndoe',
-    data:{
+  siteMetadata: {
+    title: "BackRoads",
+    description:
+      "Explore awesome worldwide tours & discover what makes each of them unique. Forget your daily routing & say yes to adventure",
+    author: "@johndoe",
+    data: {
       name: "name example",
       age: 24,
-    }
+    },
   },
   plugins: [
     {
@@ -16,10 +21,17 @@ module.exports = {
         path: `${__dirname}/src/images/`,
       },
     },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-sass`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-transition-link`,
-  ]
+  ],
 }
